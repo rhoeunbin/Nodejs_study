786d74e2d8e738f5a892c5129b5d3ede69dadbf6
rhoeunbin <rhoeb21@gmail.com>
2023-02-11 01:12:49 +0900

    node.js api 공부(조코딩)

--- n0209/index.js
+++ n0209/index.js
@@ -25,8 +25,11 @@
 // app.listen(3000)
 
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = 3000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -38,6 +41,45 @@ app.get('/dog', (req, res) => {
 
 app.get('/cat', (req, res) => {
   res.send('고양이')
+})
+
+app.get('/user/:id', (req, res) => {
+  // const q = req.params
+  // console.log(q)
+  const q = req.query
+  console.log(q)
+  
+  console.log(q.q)
+  console.log(q.name)
+  res.json({'userid': q.name})
+})
+
+// app.get('/sound/:name', (req, res) => {
+//   // const p = req.params
+//   // port.name
+  
+//   const { name } = req.params
+//   console.log(name)
+
+//   res.json({'sound': '야옹'})
+// })
+
+app.get('/sound/:name', (req, res) => {
+  // const p = req.params
+  // port.name
+  
+  const { name } = req.params
+  console.log(name)
+
+  if (name == "dog") {
+      res.json({'sound': '멍멍'})
+  } else if (name == "cat") {
+      res.json({'sound': '야옹'})
+  } else if (name == "pig") {
+      res.json({'sound': '꿀꿀'})
+  } else {
+      res.json({'sound': '알수없음'})
+  }
 })
 
 app.listen(port, () => {
